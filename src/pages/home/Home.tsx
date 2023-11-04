b106b9bf8580bc7f65d4d855b15d253581ed2b81
Roman Mettler <roman.mettler@gmail.com>
2023-11-04 13:26:39 -0300

    Set feature flags in Homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,31 @@
+import { useFeatureFlag } from 'configcat-react';
+
 export default function Home() {
+	const { value: value1, loading: loading1 } = useFeatureFlag<boolean>(
+		'featureflag1',
+		false,
+	);
+	const { value: value2, loading: loading2 } = useFeatureFlag(
+		'featureflag2',
+		false,
+	);
+	const { value: value3, loading: loading3 } = useFeatureFlag(
+		'featureflag3',
+		false,
+	);
+
 	return (
 		<div className="flex items-center justify-center h-screen">
 			{/* data-test? https://docs.cypress.io/guides/references/best-practices */}
 			<h1 className="text-3xl font-bold underline" data-test="home-msg">
 				This is the homepage!
+				{!loading1 && !loading2 && !loading3 && (
+					<>
+						{value1 && <div>valor1: {value1}</div>}
+						{value2 && <div>valor2: {value2}</div>}
+						{value3 && <div>valor3: {value3}</div>}
+					</>
+				)}
 			</h1>
 		</div>
 	);
